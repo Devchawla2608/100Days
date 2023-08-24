1189ffb69ce765eb907965a30ba424e583454eb0
Deepanshu Chawla <dev.chawla2608@gmail.com>
2023-08-24 22:53:07 +0530

    Day51

---
+++ Day51/LeetcodeDaily.cpp
@@ -0,0 +1,69 @@
+
+
+
+class DisjointSet{ 
+    public:
+    vector<int> rank , size , parent;
+    DisjointSet(int n){
+        rank.resize(n+1 , 0);
+        size.resize(n+1 , 1);
+        parent.resize(n+1);
+        for(int i = 0;i<=n; i++){
+            parent[i] = i;
+        }
+    }
+    int findUParent(int node){
+        if(node == parent[node]) return node;
+        return parent[node] = findUParent(parent[node]);
+    }
+    
+    void unionBySize(int u , int v){
+        int ulp_u = findUParent(u);
+        int ulp_v = findUParent(v);
+        if(ulp_u == ulp_v) return;
+        if(size[ulp_u] < size[ulp_v]){
+            parent[ulp_u] = ulp_v;
+            size[ulp_u] += size[ulp_v];
+        }else{
+            parent[ulp_v] = ulp_u;
+            size[ulp_v] += size[ulp_u];
+        }
+    }
+    
+    void unionByRank(int u , int v){
+        int ulp_u = findUParent(u);
+        int ulp_v = findUParent(v);
+        if(ulp_u == ulp_v) return;
+        if(rank[ulp_u] < rank[ulp_v]){
+            parent[ulp_u] = ulp_v;
+        }else if(rank[ulp_u] > rank[ulp_v]){
+            parent[ulp_v] = ulp_u;
+        }else{
+            parent[ulp_u] = ulp_v;
+            rank[ulp_v]++;
+        }
+    }
+    
+};
+
+
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& arr) {
+        int V = arr.size();
+        DisjointSet ds(V);
+        for(int i = 0;i<V ; i++){
+            for(int j = 0;j<V ; j++){
+                if(arr[i][j] == 1){
+                    ds.unionBySize(i , j);
+                }
+            }
+        }
+        int count = 0;
+        for(int i = 0;i<V ; i++){
+            if(i == ds.parent[i]) count++; 
+        }
+        return count;
+    }
+
+};
