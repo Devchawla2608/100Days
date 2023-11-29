12e2d85e4069df4ca79ff542b7c65a03283f4401
Deepanshu Chawla <dev.chawla2608@gmail.com>
2023-11-29 23:48:00 +0530

    Day147

---
+++ Day147/GfgDaily.cpp
@@ -0,0 +1,16 @@
+int isEulerCircuit(int V, vector<int>adj[]){
+        int t =0;
+        for(int i=0;i<V;i++){
+            int z = adj[i].size();
+            if(z%2!=0){
+                t++;
+            }
+        }
+        if(t==0){
+            return 2;
+        }
+        if(t==2){
+            return 1;
+        }
+        return 0;
+    }
